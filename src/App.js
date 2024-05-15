528efcb3e79b640d7a1ef7c0b5767b5d2ef563f8
ashfaquddin <ashfaquddin.wrf227@gmail.com>
2024-05-15 11:03:21 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className='text-3xl'>
+          laa
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
